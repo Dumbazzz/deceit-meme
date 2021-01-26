29f0b9224353920e413858a91652ce89c7c9a680
zerrocxste <49279293+zerrocxste@users.noreply.github.com>
2021-01-26 19:55:11 +0600

    Update dllmain.cpp

--- deciet-base/dllmain.cpp
+++ deciet-base/dllmain.cpp
@@ -103,19 +103,19 @@ namespace memory_utils
 	template<class T>
 	void write(std::vector<DWORD_OF_BITNESS>address, T value)
 	{
-		size_t lengh_array = address.size() - 1;
+		size_t length_array = address.size() - 1;
 		DWORD_OF_BITNESS relative_address;
 		relative_address = address[0];
-		for (int i = 1; i < lengh_array + 1; i++)
+		for (int i = 1; i < length_array + 1; i++)
 		{
 			if (is_valid_ptr((LPVOID)relative_address) == false)
 				return;
 
-			if (i < lengh_array)
+			if (i < length_array)
 				relative_address = *(DWORD_OF_BITNESS*)(relative_address + address[i]);
 			else
 			{
-				T* writable_address = (T*)(relative_address + address[lengh_array]);
+				T* writable_address = (T*)(relative_address + address[length_array]);
 				*writable_address = value;
 			}
 		}
@@ -124,19 +124,19 @@ namespace memory_utils
 	template<class T>
 	T read(std::vector<DWORD_OF_BITNESS>address)
 	{
-		size_t lengh_array = address.size() - 1;
+		size_t length_array = address.size() - 1;
 		DWORD_OF_BITNESS relative_address;
 		relative_address = address[0];
-		for (int i = 1; i < lengh_array + 1; i++)
+		for (int i = 1; i < length_array + 1; i++)
 		{
 			if (is_valid_ptr((LPVOID)relative_address) == false)
 				return T();
 
-			if (i < lengh_array)
+			if (i < length_array)
 				relative_address = *(DWORD_OF_BITNESS*)(relative_address + address[i]);
 			else
 			{
-				T readable_address = *(T*)(relative_address + address[lengh_array]);
+				T readable_address = *(T*)(relative_address + address[length_array]);
 				return readable_address;
 			}
 		}
@@ -144,19 +144,19 @@ namespace memory_utils
 
 	void write_string(std::vector<DWORD_OF_BITNESS>address, char* value)
 	{
-		size_t lengh_array = address.size() - 1;
+		size_t length_array = address.size() - 1;
 		DWORD_OF_BITNESS relative_address;
 		relative_address = address[0];
-		for (int i = 1; i < lengh_array + 1; i++)
+		for (int i = 1; i < length_array + 1; i++)
 		{
 			if (is_valid_ptr((LPVOID)relative_address) == false)
 				return;
 
-			if (i < lengh_array)
+			if (i < length_array)
 				relative_address = *(DWORD_OF_BITNESS*)(relative_address + address[i]);
 			else
 			{
-				char* writable_address = (char*)(relative_address + address[lengh_array]);
+				char* writable_address = (char*)(relative_address + address[length_array]);
 				*writable_address = *value;
 			}
 		}
