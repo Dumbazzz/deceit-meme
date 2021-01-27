a2b509fcc6dd3e0b7dd987cda9d32f1644134c6d
zerrocxste <49279293+zerrocxste@users.noreply.github.com>
2021-01-27 12:20:33 +0600

    Update dllmain.cpp

--- deciet-base/dllmain.cpp
+++ deciet-base/dllmain.cpp
@@ -164,19 +164,19 @@ namespace memory_utils
 
 	char* read_string(std::vector<DWORD_OF_BITNESS>address)
 	{
-		size_t lengh_array = address.size() - 1;
+		size_t length_array = address.size() - 1;
 		DWORD_OF_BITNESS relative_address;
 		relative_address = address[0];
-		for (int i = 1; i < lengh_array + 1; i++)
+		for (int i = 1; i < length_array + 1; i++)
 		{
 			if (is_valid_ptr((LPVOID)relative_address) == false)
 				return NULL;
 
-			if (i < lengh_array)
+			if (i < length_array)
 				relative_address = *(DWORD_OF_BITNESS*)(relative_address + address[i]);
 			else
 			{
-				char* readable_address = (char*)(relative_address + address[lengh_array]);
+				char* readable_address = (char*)(relative_address + address[length_array]);
 				return readable_address;
 			}
 		}
